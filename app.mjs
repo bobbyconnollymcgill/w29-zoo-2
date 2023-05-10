348b67ed5246494b2dcba5635b18099a5c180955
Bobby Connolly <thomas.connolly@mcgill.ca>
2023-05-09 20:00:43 -0400

    make port dynamic

--- app.mjs
+++ app.mjs
@@ -34,6 +34,8 @@ app.post("/cookie-example", (req, res) => {
   res.end();
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
